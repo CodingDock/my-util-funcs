d0840b825e7e233c9758c617f2787728b07d0227
肖明明 <794084226@qq.com>
2016-11-24 14:30:33 +0800

    add info

--- src/main/java/newdate/DistributionPeriodUtils.java
+++ src/main/java/newdate/DistributionPeriodUtils.java
@@ -32,6 +32,8 @@ public class DistributionPeriodUtils {
         ld=ld.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
         System.out.println(ld);
 
+        //dsdfsfs
+        
 
     }
     
